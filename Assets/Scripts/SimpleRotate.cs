3c7f9fbbf81295a5ba2c670f84bbe15c4989456a
Rohin Knight <rohin.knight@gmail.com>
2020-02-02 12:00:05 +1300

    Updated Title Screen with rotating space skybox

---
+++ Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    [SerializeField] Vector3 RotateBy = Vector3.zero;
+
+    void Update()
+    {
+        transform.Rotate(RotateBy);
+    }
+}
