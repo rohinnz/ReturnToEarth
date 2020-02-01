d6afb8d2f6b2f57b96ca78bf00b9de1b8151c520
atkint <ttatkin@gmail.com>
2020-02-01 20:40:46 +1300

    Starvation mechanic added

--- Assets/Scripts/SpawnableObject.cs
+++ Assets/Scripts/SpawnableObject.cs
@@ -18,7 +18,22 @@ public class SpawnableObject : MonoBehaviour
     public GameObject[] Prefabs;
     public float EnergyConsumption = 1f;
     public Color BarColour;
-    public float Population = 0f;
+    //public float Population = 0f;
+    public float Population
+    {
+        get
+        {
+            if (!isWater)
+            {
+                // The population is how many creatures are in the lookup list
+                return GameCore.CreationLookup[name].Count;
+            }
+            else
+            {
+                return GameCore.WaterLevel;
+            }
+        }
+    }
     public float MaxPopulation = 1f;
     public float TotalConsumptionOfMe = 0f;
 }
