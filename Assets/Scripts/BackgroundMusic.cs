271b8fd38883b42d0dcf13ace40ec226be4ed224
Rohin Knight <rohin.knight@gmail.com>
2020-02-02 15:31:08 +1300

    Setup music to play from title screen without restarting

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    static bool AudioBegin = false;
+    [SerializeField] AudioSource audioSource;
+
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            //audioSource.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+}
