4e91aa1af8a72e3691034dd5982822164aeb3ffa
atkint <ttatkin@gmail.com>
2020-02-02 14:46:48 +1300

    Started creating body counter

---
+++ Assets/Scripts/BodyCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyCounter : MonoBehaviour
+{
+    public Dictionary<string, Dictionary<string, int>> Memorial = new Dictionary<string, Dictionary<string, int>>();
+
+    private void Start()
+    {
+        
+    }
+
+    public void RecordDeath(string creature, string deathType, int number)
+    {
+        if (Memorial.ContainsKey(creature))
+    }
+}
