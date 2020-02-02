bf815cdf21642c66a7f3c09f5b2152aa9c8dae35
atkint <ttatkin@gmail.com>
2020-02-02 15:14:51 +1300

    Memorial text

--- Assets/PostgameTextController.cs
+++ Assets/PostgameTextController.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class PostgameTextController : MonoBehaviour
 {
     public TMP_Text ExplanationText;
-
+    public TMP_Text MemorialText;
     void Start()
     {
         ExplanationText.text = GameCore.EndScreenText;
+        foreach(KeyValuePair<string,Dictionary<string,int>> kv in BodyCounter.Memorial)
+        {
+            foreach(KeyValuePair<string,int> outcome in kv.Value)
+            {
+                MemorialText.text += outcome.Value + " " + kv.Key + " " + outcome.Key;
+            }
+        }
     }
 }
